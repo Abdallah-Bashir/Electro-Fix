11aabde73a532f6016612cbacc4c888f11f894ec
Abdallah <Abdallah@gmail.com>
2023-11-28 02:41:36 +0200

    make some changes

---
+++ Implementation/src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+import badge from "../badgee.png.png";
+import { Link } from "react-router-dom";
+import "../styles/Navigation.css";
+
+function Navigation() {
+  return (
+    <div>
+      {" "}
+      <header>
+        <div className="header-left">
+          <img src={badge} alt="badge" style={{ width: "10em" }}></img>
+        </div>
+        <div className="header-right">
+          <Link to="/services">Services</Link>
+          <Link to="/Signup">Sign Up / Login</Link>
+          <Link to="/TechSign" className="fixer-button">
+            Become a Fixer
+          </Link>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Navigation;
